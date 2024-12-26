b561ec25b711d0a3ec13af6bc305516785944226
JEVizcaya <jevizcaya@hotmail.com>
2024-12-26 19:47:35 +0100

    todo enlazado

--- js/registro.js
+++ js/registro.js
@@ -1,6 +1,7 @@
 var inputPass=document.getElementById("password")
 var inputRePass=document.getElementById("repassword")
 var btnCrear=document.getElementById("btnCrear")
+var msg=document.getElementById("msg")
 
 inputPass.oninput=validacionInput;
 inputRePass.oninput=validacionInput;
@@ -13,8 +14,10 @@ function validacionInput(){
     var txtRepass=inputRePass.value;
     if(txtPass==txtRepass){
         btnCrear.disabled=false;
+        msg.style.display="none";
     }else{
         btnCrear.disabled=true;
+        msg.style.display="block";
     }
     //console.log(txtPass);
 } 
