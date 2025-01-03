170951e1ea632a5624f30418b794f2992252398c
JEVizcaya <jevizcaya@hotmail.com>
2025-01-03 20:09:58 +0100

    incidencias terminado

--- js/main.js
+++ js/main.js
@@ -26,7 +26,60 @@ for (let index = 0; index < papeleras.length; index++) {
     element.onclick=function(e){
         e.target.parentElement.parentElement.remove();
        // alert("borrando")
+       //var row=this.closest("tr");
+       //row.remove(); seria otra forma de hacerlo
     };
 
     
+}
+//para poner la fecha actual en el input de fecha
+document.getElementById("fecha").value=new Date().toISOString().substring(0,10);
+
+var formulario=document.getElementById("formIncidencias");
+formulario.onsubmit=function(e){
+    e.preventDefault();
+    var incidencia=document.getElementById("descripcion").value;
+    var fecha=document.getElementById("fecha").value;
+    var tabla=document.getElementById("tbodyIncidencias");
+    var tr=document.createElement("tr");
+    var td1=document.createElement("td");
+    var td2=document.createElement("td");
+    var td3=document.createElement("td");
+    var td4=document.createElement("td");
+    var i=document.createElement("i");
+    i.classList.add("fa-solid");
+    i.classList.add("fa-trash");
+    i.onclick=function(e){
+        var row=this.closest("tr");
+        row.remove();
+    };
+    td1.innerText=getNextId("tablaIncidencias");
+    td2.innerText=formatearFecha(fecha);
+    td3.innerText=incidencia;
+    td4.appendChild(i);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tabla.appendChild(tr);
+    formulario.reset();
+    document.getElementById("fecha").value=new Date().toISOString().substring(0,10);
+};
+
+function formatearFecha(fechaOriginal) {
+    var datosFecha=fechaOriginal.split("-");
+    return datosFecha[2]+"/"+datosFecha[1]+"/"+datosFecha[0];
+}
+
+function getNextId(idTabla) {
+    var tablaIncidencia = document.getElementById(idTabla);
+    var rows = tablaIncidencia.getElementsByTagName('tr');
+    var maxId = 0;
+    for (var i = 1; i < rows.length; i++) { // Start from 1 to skip the header row
+        var id = parseInt(rows[i].getElementsByTagName('td')[0].innerText);
+        if (id > maxId) {
+            maxId = id;
+        }
+    }
+    return maxId + 1;
 }
